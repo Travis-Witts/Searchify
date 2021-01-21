724b9b956ecabd3e943299330998fe82a68ed90a
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-01-21 20:48:15 +1030

    feat: Add Spotify API methods

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 // Define global variables
 // =======================
 
-
 // =======================
 
 // UI Functions
@@ -31,23 +30,53 @@
 // Ticketmaster API search function
 // -----------------------
 
-// -----------------------
-
-// 
-
 // Spotify API artist search function
 // -----------------------
 
-// -----------------------
+// Spotify API Auth Service
+function spotifyAuthService() {
+  const queryURL = "https://accounts.spotify.com/api/token";
+  const clientKey =
+    "OTIwMTI1MTA1ODZjNDllY2FjYWRkNjg3MTNjYzdhMmU6MjEzNTg0MTBhMzE4NDJhY2E1Mzc2YTFhMzcyNmJmYTY=";
 
-// Youtube API search Function
-// -----------------------
+  return $.ajax({
+    url: queryURL,
+    method: "POST",
+    data: {
+      grant_type: "client_credentials",
+    },
+    headers: {
+      Authorization: `Basic ${clientKey}`,
+    },
+  }).then(function (response) {
+    // Return Access Token
+    return response.access_token;
+  });
+}
 
-// -----------------------
+// Spotify Search Artist by Name
+async function spotifyGetArtistService(name) {
+  const token = await spotifyAuthService();
+  const queryURL = "https://api.spotify.com/v1/search";
+  let data;
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+    data: {
+      q: name,
+      type: "artist",
+    },
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }).then((response) => {
+    console.log(response);
+  });
+}
 
 // =======================
 
-// Listeners go down here 
+// Listeners go down here
 
 // =======================
 
@@ -62,3 +91,5 @@
 // -----------------------
 
 // =======================
+
+// spotifyGetArtistService();
